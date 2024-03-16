d03fe979ad24a60243830e9b3a7bc5e2ff604ad3
LytovchenkoOlenka <131744554+LytovchenkoOlenka@users.noreply.github.com>
2024-03-16 20:46:28 +0100

    ааа

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,2 +1,11 @@
 import Swiper from 'swiper';
 import 'swiper/css';
+
+const swiper = new Swiper('.swiper', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+swiper();
