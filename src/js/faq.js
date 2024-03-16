7d961a1266c60411463df160d00f6060cb270efc
OleksandrMiedviediev <119736072+OleksandrMiedviediev@users.noreply.github.com>
2024-03-16 02:26:21 +0100

    Update faq.js

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,5 +1,22 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
-new Accordion('.accordion-container', {
+
+const accordion = new Accordion('.accordion-container', {
   showMultiple: true,
+});
+
+const accordionItems = document.querySelectorAll('.faq-item');
+
+accordionItems.forEach(item => {
+  const header = item.querySelector('.ac-header');
+  const button = item.querySelector('.ac-trigger');
+  const content = item.querySelector('.ac-text');
+
+  header.addEventListener('click', () => {
+    if (content.classList.contains('active')) {
+      accordion.close(content);
+    } else {
+      button.click();
+    }
+  });
 });
