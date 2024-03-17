0aa771b4b3995f1a3fe84ef49c5871d8b404174c
Halenina <galenina20032101@gmail.com>
2024-03-17 15:00:46 +0200

    add js

---
+++ src/js/hero.js
@@ -0,0 +1,21 @@
+const Instargam = document.getElementById('IG');
+const Facebook = document.getElementById('FB');
+const YouTube = document.getElementById('YT');
+const GitHub= document.getElementById('GH');
+
+Instargam.addEventListener('click', () => {
+  window.location.href = 'https://www.instagram.com/goitclub/';
+});
+
+Facebook.addEventListener('click', () => {
+  window.location.href = 'https://www.facebook.com/goITclub/';
+});
+
+YouTube.addEventListener('click', () => {
+  window.location.href = 'https://www.youtube.com/c/GoIT';
+});
+
+GitHub.addEventListener('click', () => {
+  window.location.href = 'https://github.com/Jey2507/teamwork-eighteen-js';
+});
+
