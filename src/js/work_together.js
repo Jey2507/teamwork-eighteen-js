21e9bee9a64c24347a4a8d4196460a41e30e670c
ghettoripper <ap4ixba@gmail.com>
2024-03-17 03:41:33 +0100

    85% done

--- src/js/work_together.js
+++ src/js/work_together.js
@@ -1,27 +1,31 @@
-document.getElementById('contactForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    
-    const email = document.getElementById('email');
+const form = document.getElementById('contactForm');
 
-    fetch('https://example.com/api/requests', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            email: email.value
-        })
-    })
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const email = form.elements.email.value;
+  const comments = form.elements.comments.value;
+
+  fetch('https://portfolio-js.b.goit.study/api/requests', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      email: email,
+      comments: comments,
+    }),
+  })
     .then(response => response.json())
     .then(data => {
-        if (data.success) {
-            document.getElementById('modal').style.display = 'block';
-            email.value = '';
-        } else {
-            alert('Помилка при відправці заявки. Будь ласка, спробуйте ще раз.');
-        }
+      if (data.success) {
+        document.getElementById('modal').style.display = 'block';
+        form.reset()
+      } else {
+        alert('Помилка при відправці заявки. Будь ласка, спробуйте ще раз.');
+      }
     })
     .catch(error => {
-        alert('Помилка при відправці заявки. Будь ласка, спробуйте ще раз.');
+      alert('Помилка при відправці заявки. Будь ласка, спробуйте ще раз.');
     });
 });
