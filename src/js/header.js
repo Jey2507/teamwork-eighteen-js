f60e65bae14ce53a4a5612f39cb6660dccd89ca5
AnnaRossova <rossova.anna@gmail.com>
2024-03-22 17:42:20 +0200

    run

--- src/js/header.js
+++ src/js/header.js
@@ -106,4 +106,21 @@ function createLinks() {
   }
 }
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+
+      const targetId = this.getAttribute('href');
+      const targetElement = document.querySelector(targetId);
+
+      if (targetElement) {
+          const offsetTop = targetElement.offsetTop;
+          window.scroll({
+              top: offsetTop,
+              behavior: 'smooth' 
+          });
+      }
+  });
+});
+
 
